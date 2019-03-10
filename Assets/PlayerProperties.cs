15b2579127286f40d38972b39c5cf01c117fd187
Hunter <hbova@mail.pima.edu>
2019-03-10 15:16:22 -0700

    Added arena prefab
    Also started photon code

---
+++ Assets/PlayerProperties.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using Photon.Pun;
+using Photon.Realtime;
+
+
+public class PlayerProperties : MonoBehaviourPun
+{
+
+    public float[] playerIncomes;
+    public float currentIncome { get; set; }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    [PunRPC]
+    // synchronous assignment of income to the player from the "server"
+    public void SetIncome(int order)
+    {
+        currentIncome = playerIncomes[order];
+    }
+}
